58fc1e48a7615327d815da79a3c305aae3610fb0
emily <1969863742@qq.com>
2018-12-13 22:08:48 +0800

    add

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -87,7 +87,7 @@ public class CategoryManageController {
         if(iUserService.checkAdminRole(user).isSuccess()){
             //是管理员
             //查询当前节点的id和递归子节点的id
-            // 0 -> 1000 -> 10000
+            // 0 -> 1000 -> 1000
             return iCategoryService.selectCategoryAndChildrenById(categoryId);
         }else {
             return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
